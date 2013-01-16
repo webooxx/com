04e6d61ccaa9a5fc41a3c813c11ce84c65660da1
徐振华 <webooxx@gmail.com>
2013-01-16 16:17:16 +0800

    重新梳理了框架结构
    Signed-off-by: 徐振华 <webooxx@gmail.com>

--- app-doc/copy_to_index/index.php
+++
@@ -1,6 +0,0 @@
-<?php
-define('_DIR_',dirname($_SERVER['SCRIPT_FILENAME']));
-define('_MVC_',_DIR_.'/ooxx.php');
-define('_CFG_',_DIR_.'/conf.php');
-$app = include_once(_MVC_);$app->run( include_once(_CFG_),$argv );
-?>
