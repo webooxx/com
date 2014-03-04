445ae9ac079a92338b799e4c3ed6ee159d9e907f
徐振华 <webooxx@gmail.com>
2014-03-04 14:29:19 +0800

    重构Model
    开始重构Model

--- com/dev/Model.php
+++ com/dev/Model.php
@@ -1,12 +1,43 @@
 <?php
-#    数据模型管理器
+#    数据模型基类
+#       数据模型类依然使用 ox:m 来进行管理，这里只用于构建基本的模型
+
 class Model{
-    private static $instaces;
-    public  static function getInstace( $table = Null , $engine = Null ){
-        $engine  = ( $engine ? $engine : C('DB_ENGINE') ).'Model';
-        $table   = $table  ? $table  : C('SYS_CURRENT_MOD');
-        #    缓存模型对象，免得每次都新建一个
-        $instace =  Model::$instaces[$engine] ?  Model::$instaces[$engine] : Model::$instaces[$engine] = new $engine;
-        return $instace->table($table);
+
+    private $prefix ;
+    private $tablename ;
+    private $dbname ;
+    private $link ;
+
+    #   连接&初始化
+    private function linkDb(){
+
     }
+
+    #   初始化构建,用于 new Model() 的情况
+    function __construct( $config ){
+
+    }
+
+    #   选择数据库
+    function db(){
+
+    }
+
+    #   设置表名
+    function table(){
+
+    }
+
+    #   设置前缀
+    function pre(){
+        
+    }
+
+
+}
+
+#   处理 mysql 的单例请求,模型文件的请求
+function M( $name , $schem = 'mysql'){
+
 }
