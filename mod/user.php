5c259895711cd12bd235fd1d035e7729981c7f49
xuzhenhua <xuzhenhua@baidu.com>
2014-09-02 14:55:26 +0800

    修正User数据模型

--- mod/user.php
+++ mod/user.php
@@ -1,6 +1,6 @@
 <?php
 
-class user extends CsvModel{
+class user extends MysqlModel{
 
     function __construct(){
     }
@@ -21,6 +21,36 @@ class user extends CsvModel{
             '3' => '编辑',
             '9' => '管理员',
         );
+    }
+
+    function findByUserName() {
+        $where['username'] = $this->getUuapUser();
+        $user              = $this->where($where)->find();
+
+        if (!$user) {
+            $data['username'] = $where['username'];
+            $data['ct']       = date('Y-m-d H:i:s');
+
+            $this->debug(0)->add($data);
+            $user = $this->where($where)->debug(0)->find();
+        }
+        return $user;
+    }
+
+    #   获得 uuap 账户信息
+    function getUuapUser() {
+        if (ENV == 'online') {
+            require('./bae_config.php');
+            require_once(BAE_LIB_PATH . '/BaeCasSession.class.php');
+            BaeCasConfigure::setConf('10.36.53.6', 'jsfeedbackdb_w', 'N2tstLHxZVIg3hHn', 'ns_bae_jsfeedbackdb', '4551');
+            $obj = BaeCasSession::getInstance('uuap.baidu.com', 443);
+            if(!$obj->isAuth()){$obj->auth(); };
+            return $obj->user();
+        }
+        else {
+            I('uuap-php/develop');
+            return phpCAS::getUser();
+        }
     }
 
 }
