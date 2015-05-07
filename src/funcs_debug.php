d062f80bce5f18d1baed5f8d8de150febf48aaf1
xuzhenhua <xuzhenhua@baidu.com>
2015-05-07 09:40:01 +0800

    调试函数更新
    输出增加了charset,新增了p

--- src/funcs_debug.php
+++ src/funcs_debug.php
@@ -1,6 +1,7 @@
 <?php  #   开发调试函数
-function dump ($arg) { @header("Content-type:text/html"); echo '<pre>'; var_dump($arg) ; echo '</pre>' ; }
-function ddump($arg) { @header("Content-type:text/html"); echo '<pre>'; var_dump($arg) ; die( '</pre>'); }
-function json ($arg) { @header("Content-type:text/json"); echo json_encode($arg) ; }
-function djson($arg) { @header("Content-type:text/json"); die( json_encode($arg)); }
+function dump ($arg) { @header("Content-type:text/html;charset=utf-8"); echo '<pre>'; var_dump($arg) ; echo '</pre>' ; }
+function ddump($arg) { @header("Content-type:text/html;charset=utf-8"); echo '<pre>'; var_dump($arg) ; die( '</pre>'); }
+function json ($arg) { @header("Content-type:text/json;charset=utf-8"); echo json_encode($arg) ; }
+function djson($arg) { @header("Content-type:text/json;charset=utf-8"); die( json_encode($arg)); }
 function olog( $msg , $level = 1 , $show = 0){ return ox::l($msg,$level,$show); }
+function p($arg) { @header("Content-type:text/html"); echo '<pre>'; print_r($arg) ; die( '</pre>'); }
